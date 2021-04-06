a48bcfed6c447c321bc29e9ac51f1aee43294911
kangmin5505 <kangmin5505@naver.com>
2021-04-06 18:40:31 +0900

    2021/04/06 commit

---
+++ DataStructure/[BOJ]10828.py
@@ -0,0 +1,28 @@
+def function(command):
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+
+
+n = int(input())
+
+stack = []
+for _ in range(n):
+    command = input().split()
+    function(command)
