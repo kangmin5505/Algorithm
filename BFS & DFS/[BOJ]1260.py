01d41dcad7237775b4e5d9f69eaa8af7cf499517
kangmin5505 <kangmin5505@naver.com>
2021-04-15 17:50:11 +0900

    2021/04/15 commit

---
+++ BFS & DFS/[BOJ]1260.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n, m, v = map(int, input().split())
+graph = [[0] * (n+1) for _ in range(n+1)]
+visited = [False] * (n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+
+def dfs(v):
+    visited[v] = True
+    print(v, end=' ')
+    for i in range(1, n+1):
+        if not visited[i] and graph[v][i] == 1:
+            dfs(i)
+
+
+def bfs(v):
+    queue = deque()
+    queue.append(v)
+    visited[v] = False
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in range(1, n+1):
+            if visited[i] and graph[v][i] == 1:
+                queue.append(i)
+                visited[i] = False
+
+
+dfs(v)
+print()
+bfs(v)
