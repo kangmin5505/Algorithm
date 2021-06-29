d96ea50d6bf3a72d7e01f07be61be12377bd59d6
kangmin5505 <74703501+kangmin5505@users.noreply.github.com>
2021-06-29 23:51:35 +0900

    2021/06/29 commit

---
+++ Pgm/[Pgm]주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    length = len(prices)
+    answer = [0] * length
+    for i in range(length):
+        for j in range(i+1, length):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+                
+    return answer
